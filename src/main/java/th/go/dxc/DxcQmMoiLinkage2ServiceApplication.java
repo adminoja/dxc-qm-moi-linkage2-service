b9dccf444b07a5adbf6bf8c2243ca126c832a5a7
Pichada Sungthong <pichada@clearskysolutions.co.th>
2025-10-01 10:49:18 +0700

    Add .yml and folder resources / Edit pom

---
+++ src/main/java/th/go/dxc/DxcQmMoiLinkage2ServiceApplication.java
@@ -0,0 +1,13 @@
+package th.go.dxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxcQmMoiLinkage2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DxcQmMoiLinkage2ServiceApplication.class, args);
+	}
+
+}
